2d207876cb1d8e69fdde4a57c1ab73087c7d0e99
karlovcihlar <karlo-valentin.cihlar@fer.hr>
2021-07-26 22:45:57 +0200

    minor fixes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,6 +5,7 @@ import Home from "./Home"
 import About from "./About"
 import Login from "./Login"
 import NavBar from "./NavBar"
+import Member from "./Member"
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {BrowserRouter, Route, Link} from "react-router-dom"
 
@@ -15,6 +16,7 @@ function App() {
       <Route exact path="/" component={Home} />
       <Route exact path="/login" component={Login} />
       <Route exact path="/about" component={About} />
+    <Route exact path="/member" component={Member} />
     </BrowserRouter>
   );
 }
