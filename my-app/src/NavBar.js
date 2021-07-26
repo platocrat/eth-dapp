a7bafcba61c399247092bf3e2befae924848f9ac
maculjak <mculyak@gmail.com>
2021-07-26 23:00:27 +0200

    fix navbar spacing

--- my-app/src/NavBar.js
+++ my-app/src/NavBar.js
@@ -7,7 +7,7 @@ class NavBar extends Component{
     return(
         <div>
         
-              <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+              <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
                 <a class="navbar-brand" href="#"> <b>cryptoBROKE.rs</b></a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
