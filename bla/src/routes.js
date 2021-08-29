1fcbf754628cc2ff45ba66fa0fc471e8a303d26f
uxpob <uxpob@student.kit.edu>
2021-08-29 18:01:47 +0200

    login changes

--- bla/src/routes.js
+++ bla/src/routes.js
@@ -1,4 +1,5 @@
 import { Navigate, useRoutes } from 'react-router-dom';
+import { useState } from 'react';
 // layouts
 import DashboardLayout from './layouts/dashboard';
 import LogoOnlyLayout from './layouts/LogoOnlyLayout';
@@ -14,8 +15,42 @@ import NotFound from './pages/Page404';
 import ToL2 from './pages/ToL2';
 
 // ----------------------------------------------------------------------
+const parseJwt = (token) => {
+  try {
+    return JSON.parse(atob(token.split('.')[1]));
+  } catch (e) {
+    return null;
+  }
+};
 
-export default function Router() {
+export default function Router(props) {
+  const [state, setState] = useState(undefined);
+  console.log(state);
+  const handleLoggedIn = (auth) => {
+    const payload = parseJwt(auth['accessToken'])['payload'];
+    window.localStorage['username'] = payload['username'];
+    window.localStorage['lastName'] = payload['lastName'];
+    window.localStorage['firstName'] = payload['firstName'];
+    window.localStorage['publicAddress'] = payload['publicAddress'];
+    window.localStorage['email'] = payload['email'];
+    window.localStorage['organisation'] = payload['organization'];
+    window.localStorage['id'] = payload['id'];
+    window.localStorage['jwt'] = auth['accessToken'];
+
+    setState(payload);
+  };
+
+  const handleLoggedOut = () => {
+    localStorage.removeItem('username');
+    localStorage.removeItem('lastName');
+    localStorage.removeItem('firstName');
+    localStorage.removeItem('publicAddress');
+    localStorage.removeItem('email');
+    localStorage.removeItem('organisation');
+    localStorage.removeItem('id');
+    localStorage.removeItem('jwt');
+    setState(undefined);
+  };
   return useRoutes([
     {
       path: '/dashboard',
@@ -33,9 +68,12 @@ export default function Router() {
       path: '/',
       element: <LogoOnlyLayout />,
       children: [
-        { path: 'home', element: <Home /> },
-        { path: 'login', element: <Login /> },
-        { path: 'register', element: <Register /> },
+        {
+          path: 'login',
+          element:
+            state !== undefined ? <Navigate to={'/home'} /> : <Login onLoggedIn={handleLoggedIn} />
+        },
+        { path: 'home', element: <Home onLoggedOut={handleLoggedOut} /> },
         { path: '404', element: <NotFound /> },
         { path: '/', element: <Navigate to="/dashboard" /> },
         { path: '*', element: <Navigate to="/404" /> }
